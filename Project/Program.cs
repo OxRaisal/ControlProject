c798c0db8cda2008ce0130438f40d4f623333f0b
Raisal <Raisal12312@mail.ru>
2022-11-27 23:52:24 +0300

    CreateStringArray

---
+++ Project/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+// string[] stringArray = {"hello", "2", "world",":-)"};
+// string[] stringArray = {"Russia", "Denmark", "Kazan"};
+// string[] stringArray = {"1234", "1567", "-2", "computer science"};
+
+string[] CreateUserStringArray(int length)
+{
+    string[] array = new string[length];
+
+    for(int i = 0; i < length; i++)
+    {
+        Console.Write("\nВведите элемент массива: ");
+        string? value = Console.ReadLine();
+        while(value == String.Empty)
+        {
+            Console.Write("\nВы не ввели значение. Введите элемент массива: ");
+            value = Console.ReadLine();
+        }
+        if(value != null) array[i] = value;
+    }
+    return array;
+}
+
+string[] CreateStringArray(string[] array, int numberSymbol = 3)
+{
+    int lengthNewArray = 0;
+    for(int i = 0; i < array.Length;i++)
+        if(array[i].Length <= numberSymbol) lengthNewArray++;
+
+    string[] newArray = new string[lengthNewArray];
+
+    for(int i = 0, j = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= numberSymbol)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
